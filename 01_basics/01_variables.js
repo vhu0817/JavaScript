a1fb3356f02ca1f24e5488efbe47d2a8e6ca8b14
Sarthak Kumar Maiti <sarthakkumarmaiti@gmail.com>
2025-12-22 16:57:15 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "sarthak@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+
+// accountId = 2 // not possible
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+The let and var has historical significance only
+Now only let and const is used
+*/
+
+// ';' doesnt matter at end of statement in js
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
