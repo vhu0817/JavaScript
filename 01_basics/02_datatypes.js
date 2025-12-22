658b379dcbb7428eb1a840e99f12a8faa242d057
Sarthak Kumar Maiti <sarthakkumarmaiti@gmail.com>
2025-12-22 16:56:08 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+"use strict"; // treat all JS code as newer version
+// however now all the code is treated as new code
+
+// alert(3 + 3) // used in browser engine, gives a pop-up as 6, this doesnt work here
+
+// console.log(3 + 3) console.log("Sarthak") // error
+console.log(3 + 3); console.log("Sarthak") // Bad readability
+let age = 18
+console.log(typeof "Sarthak")
+console.log(typeof age)
